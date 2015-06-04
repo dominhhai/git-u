ce150251f2088d3b6c47758fe641943f7f6b8f5e
Hai Do Minh <minhhai3b@gmail.com>
2015-06-04 21:02:01 +0900

    add helper command

--- cli.js
+++ cli.js
@@ -2,4 +2,13 @@
 
 const cloner = require('./app')
 
+if (process.argv.length < 4) {
+	console.error("usage: git-u [command] owner repository\n\nwhere:")
+	console.error("    command        the gits command. Default is clone")
+	console.error("    owner          the repository's owner name")
+	console.error("    repository     the repository's name")
+
+	return -1
+}
+
 cloner.apply(null, process.argv.slice(2))
